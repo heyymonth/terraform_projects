331cb3267987231273b926be3bc7818000c354ea
heyymonth <heyymonth@gmail.com>
2021-07-09 15:17:12 +0530

    ec2 instance 1st commit

---
+++ 02-EC2-instance-with-ASG/main.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.ec2-instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "ec2-instance" {
+  name = "terraform-example-instance"
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "SG for ec2"
+    from_port   = 8080
+    protocol    = "tcp"
+    to_port     = 8080
+  }
+}
